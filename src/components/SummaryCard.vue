98de31d883654d24ef32f792d7274e0e2b7995b7
jamone- <dominik.rabij@gmail.com>
2017-07-05 22:56:13 +0200

    summary-card almost done

--- src/components/SummaryCard.vue
+++ src/components/SummaryCard.vue
@@ -1,38 +1,76 @@
 <template>
   <div class="card">
     <div class="header">
+      <span>{{ user }}:</span> <span :class="{ green: summary.sum > 0, red: summary.sum < 0 }">{{ balance }}</span>
     </div>
-    <div class="content">
+    <div class="empty-entries" v-if="!summary.entries.length">
+      <p>Nic tu nie ma, czysto :)</p>
+    </div>
+    <div class="entries" v-else>
+      <div class="entry" v-for="(entry, i) in summary.entries" :key="i">
+        <span>{{ entry.from }}</span> <span>&rarr;</span> <span>{{ entry.to }}</span> <span class="entry-amount" :class="{ green: entry.to == user, red: entry.from == user }">{{ entry.amount }} zł</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      user: 'User',
-      balance: 123.45
-    };
+  props: ['user', 'summary'],
+  computed: {
+    balance() {
+      let amount = this.summary.sum;
+
+      if (amount < 0) {
+        amount = Number(-amount).toFixed(2);
+        return `−${amount} zł`;
+      }
+      else
+        return `${amount} zł`;
+    }
   }
 }
 </script>
 
 <style scoped>
   .card {
-    width: 300px;
-    border: 3px solid black;
+    width: 330px;
+    margin-bottom: 30px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, .1), 0px 0px 4px rgba(0, 0, 0, .1);
   }
 
   .header {
     width: 100%;
-    height: 200px;
-    border-bottom: 5px dashed black;
+    padding: 10px;
+    border-bottom: 1px dashed rgba(44, 62, 80, 0.6);
+    font-size: 25px;
+    font-weight: 300;
+    display: flex;
+    justify-content: space-between;
   }
 
-  .content {
-    width: 100%;
-    height: 100px;
+  .entries {
+    padding: 10px;
+  }
+
+  .entry {
+    display: grid;
+    grid-template-columns: 1fr 40px 1fr 1fr;
+    grid-template-rows: 100%;
+    font-size: 16px;
+    font-weight: 300;
+  }
+
+  .entry-amount {
+    text-align: right;
+  }
+
+  .green {
+    color: #1abc9c;
+  }
+
+  .red {
+    color: #e67e22;
   }
   
 </style>
--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,15 +1,90 @@
 <template>
   <div>
-    <p>MainPage</p>
+    <div class="summaries">
+      <summary-card v-for="(summary, user) in summaries"
+        :key="user"
+        :user="user"
+        :summary="summary">
+      </summary-card>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import SummaryCard from '../SummaryCard.vue';
 
+export default {
+  components: {
+    SummaryCard
+  },
+  data() {
+    return {
+      bills: [
+        {
+          comment: 'Za jakieś duperele',
+          entries: [{ from: 'Werner', to: 'Dominik', amount: 12 }, { from: 'Werner', to: 'Rafał', amount: 17.82 }, { from: 'Dominik', to: 'Rafał', amount: 3 }]
+        }, {
+          comment: 'Dominik dłużnik',
+          entries: [{ from: 'Dominik', to: 'Rafał', amount: 19.90 }, { from: 'Dominik', to: 'Werner', amount: 10 }]
+        }
+      ]
+    };
+  },
+  computed: {
+    summaries() {
+      const summaries = this.getSummaries();
+      const printableSummaries = {};
+      const formatNumber = n => Number(n).toFixed(2);
+
+      for (const from in summaries) {
+        printableSummaries[from] = { sum: formatNumber(summaries[from]._sum) };
+        const entries = [];
+
+        for (const to in summaries[from]) {
+          if (to == '_sum') continue;
+
+          const amount = summaries[from][to];
+
+          if (amount < 0)
+            entries.push({ from: to, to: from, amount: formatNumber(-amount) });
+          else if (amount > 0)
+            entries.push({ from, to, amount: formatNumber(amount) });
+        }
+
+        printableSummaries[from].entries = entries;
+      }
+
+      return printableSummaries;
+    }
+  },
+  methods: {
+    getSummaries() {
+      const summaries = {};
+
+      this.bills.forEach(bill => {
+        bill.entries.forEach(entry => {
+          summaries[entry.from] = summaries[entry.from] || { _sum: 0 };
+          summaries[entry.from][entry.to] = summaries[entry.from][entry.to] || 0;
+          summaries[entry.to] = summaries[entry.to] || { _sum: 0 };
+          summaries[entry.to][entry.from] = summaries[entry.to][entry.from] || 0;
+
+          summaries[entry.from][entry.to] += entry.amount;
+          summaries[entry.from]._sum -= entry.amount;
+          summaries[entry.to][entry.from] -= entry.amount;
+          summaries[entry.to]._sum += entry.amount;
+        });
+      });
+
+      return summaries;
+    }
+  }
 }
 </script>
 
 <style>
-  
+  .summaries {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 </style>
