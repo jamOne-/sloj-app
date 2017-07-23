2489a9a24d92618a0da1103aad7cca12617f3818
jamone- <dominik.rabij@gmail.com>
2017-07-23 23:11:05 +0200

    removed cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
