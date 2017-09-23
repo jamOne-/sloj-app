b532bf1c5be6e5384b6f88930b1924e3ae91d526
jamone- <dominik.rabij@gmail.com>
2017-09-23 14:03:42 +0200

    group names

--- api/groups.js
+++ api/groups.js
@@ -1,20 +1,23 @@
 const router = require('express').Router();
 
 const possibleTos = [
-  { name: 'Dominik', users: ['Dominik'] },
-  { name: 'Werner', users: ['Werner'] },
-  { name: 'Rafał', users: ['Rafał'] }
+  { name: 'Dominikowi', users: ['Dominik'] },
+  { name: 'Wernerskiemu', users: ['Werner'] },
+  { name: 'Rafałowi', users: ['Rafał'] }
 ];
 
 const possibleFroms = [
-  ...possibleTos,
-  { name: 'Dofał', users: ['Dominik', 'Rafał'] },
+  { name: 'Dominik', users: ['Dominik'] },
+  { name: 'Werner', users: ['Werner'] },
+  { name: 'Rafał', users: ['Rafał'] },
+  { name: 'Wernik', users: ['Werner', 'Dominik'] },
   { name: 'Rafner', users: ['Werner', 'Rafał'] },
-  { name: 'Werinik', users: ['Werner', 'Dominik'] },
+  { name: 'Dofał', users: ['Dominik', 'Rafał'] },
   { name: 'Słój', users: ['Dominik', 'Rafał', 'Werner'] }
 ];
 
 const groups = possibleFroms
+  .concat(possibleTos)
   .reduce((groups, group) => (groups[group.name] = group, groups), {});
 
 router.get('/', function (req, res) {
